3df914ef900aae7b1d22aaae2c86b2d49870ab9c
guang <prasit.tang9999@gmail.com>
2020-03-02 15:37:19 +0700

    Finish 2March

---
+++ lib/utility/my_style.dart
@@ -0,0 +1,13 @@
+
+
+import 'package:flutter/material.dart';
+
+class MyStyle {
+  // Field
+  Color darkColor = Colors.pink.shade700;
+  Color primaryColor = Colors.yellow;
+
+  // Method
+  MyStyle();
+
+}
