50ddf336fd9011bc4ace2ffe15eb61e72b48fd4c
guang <prasit.tang9999@gmail.com>
2020-03-03 11:22:58 +0700

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:guangglo/widget/authen.dart';
 
 void main() {
@@ -8,8 +9,13 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations(<DeviceOrientation>[
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown
+    ]);
     return MaterialApp(
-      home:Authen(),
+      debugShowCheckedModeBanner: false,
+      home: Authen(),
     );
   }
 }
---
+++ lib/widget/register.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:guangglo/utility/my_style.dart';
+
+class Register extends StatefulWidget {
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  // Widget
+
+  // Method
+
+  Widget nameForm() {
+    return Container(child: TextField());
+  }
+
+  Widget cameraButton() {
+    return IconButton(
+      icon: Icon(
+        Icons.add_a_photo,
+        size: 36.0,
+        color: MyStyle().darkColor,
+      ),
+      onPressed: () {},
+    );
+  }
+
+  Widget galleryButton() {
+    return IconButton(
+      icon: Icon(
+        Icons.add_photo_alternate,
+        size: 36.0,
+        color: MyStyle().primaryColor,
+      ),
+      onPressed: () {},
+    );
+  }
+
+  Widget ShowButton() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: <Widget>[cameraButton(), galleryButton()],
+    );
+  }
+
+  Widget showAvartar() {
+    return Container(
+      child: Image.asset('images/avartar.png'),
+      height: MediaQuery.of(context).size.height * 0.5,
+      width: MediaQuery.of(context).size.width,
+    );
+  }
+
+  Widget registerButton() {
+    return IconButton(
+      tooltip: 'Upload to Firebase',
+      icon: Icon(Icons.cloud_upload),
+      onPressed: () {},
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: <Widget>[registerButton()],
+        title: Text('Register'),
+        backgroundColor: MyStyle().darkColor,
+      ),
+      body: ListView(
+        children: <Widget>[
+          showAvartar(),
+          ShowButton(),
+          nameForm(),
+        ],
+      ),
+    );
+  }
+}
