69d9990648c98db409c48ae38839afcabf656458
guang <prasit.tang9999@gmail.com>
2020-03-05 16:15:51 +0700

    update 5 march

--- lib/utility/my_style.dart
+++ lib/utility/my_style.dart
@@ -4,7 +4,8 @@ import 'package:flutter/material.dart';
 
 class MyStyle {
   // Field
-  Color darkColor = Color.fromARGB(0xff, 0xc4, 0x3e, 0x00);
+  // Color darkColor = Color.fromARGB(0xff, 0xc4, 0x3e, 0x00);
+  Color darkColor = Color.fromARGB(0xff, 0xc5, 0x3e, 0x00);
   Color primaryColor = Color.fromARGB(0xff, 0xff, 0x6f, 0x00);
 
   // Method
