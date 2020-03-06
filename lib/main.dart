5c732eb835c87bb4fa218d5e91bcbe977b28ff13
guang <prasit.tang9999@gmail.com>
2020-03-06 09:17:27 +0700

    Morning 6march

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
       DeviceOrientation.portraitDown
     ]);
     return MaterialApp(
+      
       debugShowCheckedModeBanner: false,
       home: Authen(),
     );
