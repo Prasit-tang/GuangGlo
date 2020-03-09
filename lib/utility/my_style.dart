c3fd1d3fa24451a27585d19d76ab0d47f9a382a4
guang <prasit.tang9999@gmail.com>
2020-03-09 15:34:39 +0700

    finish 9 march

--- lib/utility/my_style.dart
+++ lib/utility/my_style.dart
@@ -8,6 +8,9 @@ class MyStyle {
   Color darkColor = Color.fromARGB(0xff, 0xc5, 0x3e, 0x00);
   Color primaryColor = Color.fromARGB(0xff, 0xff, 0x6f, 0x00);
 
+  String urlAvatar='https://firebasestorage.googleapis.com/v0/b/guangglo.appspot.com/o/Avatar%2Favartar.png?alt=media&token=b8da7514-6ed4-4dd1-b503-e4398f2d9755';
+  
+
   // Method
   MyStyle();
 
